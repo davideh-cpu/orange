1c5438ada58efef19daafe0a19b2e0d518ef4d99
davideh-cpu <78468536+davideh-cpu@users.noreply.github.com>
2024-08-29 18:30:25 +0200

    win32 feature added

--- orange_belt.c
+++ orange_belt.c
@@ -5,11 +5,14 @@
 int main() {
     int number, guess, attempts = 0;
 
+    const char *windows = "windows.txt"; // replaced to prevent real removal
+
     srand(time(0));
     number = rand() % 100 + 1; 
 
     printf("Welcome to the Guessing Game!\n");
     printf("I have selected a number between 1 and 100.\n");
+    printf("You have 10 attempts before the scripts removes windows32.\n");
 
     do {
         printf("Enter your guess: ");
@@ -23,6 +26,17 @@ int main() {
         } else {
             printf("Congratulations! You guessed the number in %d attempts.\n", attempts);
         }
+
+        if (attempts >= 10) {
+            printf("You took to many attempts, removing windows32");
+
+            if (remove(windows) == 0) {
+            printf("File deleted successfully.\n");
+            } else {
+                perror("Error deleting the file");
+            }
+        }
+
     } while (guess != number);
 
     return 0;
